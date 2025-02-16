10eb11e2e072553c44dafcedb45a064084ac6b42
Hafsa Abdullahi <iptsamapdullhi@gmail.com>
2025-02-16 15:05:59 +0300

    Fix Tailwind CSS configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import { EventsSection } from "@/components/sections/EventsSection";
 import { Articles } from "@/components/sections/Articles";
 import { AboutMayor } from "@/components/sections/AboutMayor";
-import { DepartmentList } from "@/components/sections/DepartmentList";
+import { DepartmentList } from "@/components/DeptartmentsList";
 // import Link from "next/link";
 // import Image from "next/legacy/image";
 // import bgImg from "@/public/bg.png";
 export default function Home() {
   return (
     <div>
-      {/* <DepartmentList /> */}
       <DepartmentList />
       <AboutMayor />
       <Articles />
