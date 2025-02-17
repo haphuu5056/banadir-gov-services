d39da1872779b54bcf23c7285542b5c94a65d6a1
aisha <caishaosman053@gmail.com>
2025-02-17 22:16:02 +0300

    canshuurta file was adede

---
+++ components/sections/TaxPaymentCard.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+interface ContentItem {
+  text: string;
+  isBold?: boolean;
+}
+
+interface Section {
+  title: string;
+  content: ContentItem[];
+  image?: {
+    src: string;
+    alt: string;
+  };
+}
+
+interface TaxPaymentProps {
+  section: Section;
+}
+
+const TaxPayment: React.FC<TaxPaymentProps> = ({ section }) => {
+  return (
+    <section>
+      <h2 className="text-2xl text-[#3F479E] font-bold mb-4">{section.title}</h2>
+      <p className="mb-3 text-lg text-[#1E1E1E]">
+        {section.content.map((content, index) =>
+          content.isBold ? (
+            <strong key={index}>{content.text}</strong>
+          ) : (
+            <span key={index}>{content.text}</span>
+          )
+        )}
+      </p>
+      {section.image && (
+        <div className="relative mb-4">
+          <Image
+            src={section.image.src || "/placeholder.svg"}
+            alt={section.image.alt}
+            width={700}
+            height={400}
+            className="rounded-lg shadow-lg"
+          />
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default TaxPayment;
